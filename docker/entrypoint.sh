98666a06199ec285b1e247182e956bae34cccf9f
Steve Taylor <steve@deployhub.com>
2019-11-18 15:00:45 -0700

    expose db connection as env vars

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -16,7 +16,6 @@ export DMHOME=$PWD
 export TRILOGYHOME=$PWD
 export LD_LIBRARY_PATH=$PWD/lib:$PWD/bin
 export PATH=$PWD/lib:$PWD/bin:$PATH
-export WEBSERVER=https://console.deployhub.com
 export DM_TARGET_USER=root
 export HOME=$(getent passwd `whoami` | cut -d: -f6)
 
@@ -29,4 +28,4 @@ echo Running DeployHub Reverse Proxy
 
 curl -k -sL "$WEBSERVER/dmadminweb/EngineEvent?getkeys=Y&clientid=$CLIENTID" | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["dmodbc"])' > /opt/deployhub/engine/dm.odbc
 curl -k -sL "$WEBSERVER/dmadminweb/EngineEvent?getkeys=Y&clientid=$CLIENTID" | python3 -c 'import json,sys;obj=json.load(sys.stdin);print(obj["dmasc"])' > /opt/deployhub/engine/dm.asc
-python3 /opt/deployhub/engine/lib/dhlistener.py $WEBSERVER $CLIENTID $PWD 
+python3 /opt/deployhub/engine/lib/dhlistener.py $WEBSERVER $CLIENTID $PWD $DBSERVER $DBPORT 
